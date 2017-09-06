aae3d3e485650bba0241ac76e0b81a4dbfd51e43
Sergi <31004630+SergioGarcia1989@users.noreply.github.com>
2017-09-06 19:31:07 +0200

    Hello World
    This programs greets the user, and uses a class with his own method that ask a question to the user.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿//Sergio Garcia Balsas
+
+using System;
+public class AreYou
+{
+    public static void Question()
+    {
+        Console.WriteLine("How are you?");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Sergio Garcia Balsas
+//A simple program that say Hello, and use a class with a own method
+// for make a question.
+using System;
+public class HelloWorld
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World ");
+        AreYou.Question();
+    }
+}
